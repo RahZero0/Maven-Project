ccab1e3aa9aee5d377d9d3aaca8a4d512275f862
RahZero0 <ayushpasham@gmail.com>
2024-11-20 11:46:51 +0530

    Maven Project

---
+++ src/main/java/hello_world_demo/WelcomClass.java
@@ -0,0 +1,10 @@
+package hello_world_demo;
+
+public class WelcomClass {
+	public static void main(String[] args) {
+		System.out.println(HelloWorld());
+	}
+	public static String HelloWorld() {
+		return "This is student Ayush Reddy, 22BD1A6744";
+	}
+}
